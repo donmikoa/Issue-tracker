8c74196d77c44160fb55ccc3fe8f521e063ed31f
Michael <michael.iyaomolere@maxxpotential.com>
2019-12-12 12:41:18 -0500

    Working on the JS

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-function fetchIssues(){
-    
+function fetchIssues() {
+	var issues = JSON.parse(localStorage.getItem("issues"));
+	var issuesList = document.getElementById("issuesList");
+
+	issuesList.innerHTML = "";
+
+	for (var i = 0; i < issues.length; i++) {
+		var id = issues[i].id;
+        var desc = issues[i].description;
+        var severity = issues[i].severity;
+        var assignedTo = issues[i].assignedTo;
+        var status = issues[i].status;
+
+        issuesList.innerHTML += '<div class="well">' +
+                                '<h6> Issue ID: ' + id + '</h6>' +
+                                '<p><span class = "label label-info">' + status
+
+	}
 }
